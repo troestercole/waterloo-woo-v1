0f8d49c7b8fc11002cca11f5c0240f86ce1dd914
mts_dev4 <mts_dev4@stephensdirect.com>
2023-10-13 12:18:41 -0500

    Wiping it

--- .remarkrc.js
+++
@@ -1,7 +0,0 @@
-exports.plugins = [
-  require('remark-frontmatter'),
-  require('remark-preset-lint-consistent'),
-  require('remark-preset-lint-recommended'),
-  require('remark-preset-lint-markdown-style-guide'),
-  require('remark-preset-prettier')
-];
